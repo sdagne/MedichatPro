d52d134903e530d9f6bb4fcf01e7e1616e2ecb62
Shewan Dagne <shewan.dagne@bluewin.ch>
2025-09-09 13:36:04 +0200

    features update

--- main.py
+++ main.py
@@ -210,7 +210,7 @@ st.markdown("""
 st.markdown("""
 <div style="text-align: center; font-size: 0.9rem;">
     <p style="color: blue;">
-        RAG [Retrieval-augmented generation] project on Euron by Shewan Dagne, guided by Instructor @Sudhanshu, for hands-on AI skill-building, strengthen practical skills, and build a foundation for future AI applications.
+    RAG {Retrieval-augmented generation} project on Euron by Shewan Dagne, guided by Instructor @Sudhanshu, for hands-on AI skill-building, strengthen practical skills.
     </p>
 </div>
 """, unsafe_allow_html=True)
